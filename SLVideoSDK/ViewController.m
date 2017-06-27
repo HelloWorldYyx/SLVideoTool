4d928890ddd07937c1f579968e7992c1c45e4071
yanyx <yanyx@adnonstop.com>
2017-06-27 10:11:38 +0800

    Initial Commit

---
+++ SLVideoSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SLVideoSDK
+//
+//  Created by admin on 27/6/17.
+//  Copyright © 2017年 YYX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
