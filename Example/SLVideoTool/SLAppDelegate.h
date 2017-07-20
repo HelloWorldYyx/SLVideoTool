5bb002f5323446f40e0c66279898fb55e7763d2f
yanyx@adnonstop.com <yanyx@adnonstop.com>
2017-07-20 14:04:04 +0800

    Initial commit

---
+++ Example/SLVideoTool/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SLVideoTool
+//
+//  Created by hellowb on 07/20/2017.
+//  Copyright (c) 2017 hellowb. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
